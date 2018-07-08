7ec4f6ff03ef6a400da07c4acbadc5e93ad9b762
jollyroger222 <jollyroger2@mail.ru>
2018-07-08 16:06:11 +0300

    Revert "test2-3"
    This reverts commit e3d2a45266fc28f60b9852af8722437b888862ee.

--- java2-dz2/src/dz2.java
+++ java2-dz2/src/dz2.java
@@ -3,8 +3,6 @@ public class dz2 {
     public static void main(String[] args){
 
         System.out.println("test");
-        System.out.println("test2");
-        System.out.println("test3");
     }
 
 }
