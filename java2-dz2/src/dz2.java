df7fe8d0a9435eabe69401a9fb613f293d7931c5
jollyroger222 <jollyroger2@mail.ru>
2018-07-08 16:09:04 +0300

    test6
    tst6

--- java2-dz2/src/dz2.java
+++ java2-dz2/src/dz2.java
@@ -3,6 +3,12 @@ public class dz2 {
     public static void main(String[] args){
 
         System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test6");
+
     }
 
 }
